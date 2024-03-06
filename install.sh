ce8ec6c8ee4308fc8118dd626c0b258e81f683b3
Luke Tomlinson <luketomlinson@github.com>
2024-03-06 09:38:31 -0500

    fix dir;

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+current_dir=$(pwd)
+
 merge_zsh_config(){
   echo "Copying contents of .zshrc from $HOME/.zshrc to $script_dir/.zshrc"
   if [[ -n "$CODESPACES" && -e "$HOME/.zshrc" ]]; then
-    less "$HOME/.zshrc" >> "$script_dir/.zshrc"
+    less "$HOME/.zshrc" >> "$current_dir/.zshrc"
   fi
 }
 
