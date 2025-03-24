53f91a1fd011a3a65644a11ea02c29e4f1a09a5f
Luke Tomlinson <luketomlinson@github.com>
2025-03-24 14:33:04 -0400

    brew

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 current_dir=$(pwd)
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew update
+brew upgrade
+
+brew install helm
+
 merge_zsh_config(){
   echo "Copying contents of .zshrc $current_dir/.zshrc" $HOME/.zshrc
   if [[ -n "$CODESPACES" && -e "$HOME/.zshrc" ]]; then
